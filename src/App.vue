ef31db6aef927583772f3b8910e82cc72f4032ef
wangfengbysj <wangfeng_bysj@163.com>
2021-02-05 10:00:29 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="app">
+        <div class='container'>
+            <div class='card mt-5'>
+                <div class="card-header">头部</div>
+                <div class="card-body">内容</div>
+                <div class="card-footer">底部</div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'app',
+    components: {},
+    data() {
+        return {};
+    }
+}
+</script>
+
+<style>
+
+</style>
