3485feae72d5908f1d1bbdd4dd9e3a6555cef5d8
wangfengbysj <wangfeng_bysj@163.com>
2021-02-11 19:56:21 +0800

    mainboardpage

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -1,12 +1,288 @@
 <template>
-    <div>
-        后台首页
-    </div>
+  <div>
+    <!-- 数据统计 -->
+    <el-row :gutter="20">
+      <el-col :span="6" v-for="(item,index) in counts" :key="index">
+        <el-card class="box-card" shadow="hover">
+          <div class="d-flex align-items-center">
+            <i :class="[item.icon,item.color]"
+               class="h4 mb-0 text-white text-center mr-3"
+               style="width: 40px;height:40px;line-height: 40px">
+            </i>
+            <div>
+              <h4 class="mb-1">{{ item.num }}</h4>
+              <small class="text-muted">{{ item.desc }}</small>
+            </div>
+          </div>
+        </el-card>
+      </el-col>
+    </el-row>
+
+    <!-- 店铺、订单提示 | 统计图 -->
+    <el-row :gutter="20" class="mt-3">
+      <el-col :span="12" class="d-flex flex-column" style="height: 370px;justify-content:space-between">
+        <el-card class="box-card" shadow="never"
+                 v-for="(tip,index) in tips" :key="index">
+          <div slot="header" class="clearfix">
+            <span>{{ tip.title }}</span>
+            <el-button style="float: right; padding: 3px 0" type="text">{{ tip.desc }}</el-button>
+          </div>
+          <div class="row">
+            <div :class="tip.list.length|getCol" v-for="(tlist,listi) in tip.list" :key="listi">
+              <button class="btn btn-light w-100">
+                <h4 class="mb-1">{{ tlist.value }}</h4>
+                <small class="text-muted">{{ tlist.name }}</small>
+              </button>
+            </div>
+          </div>
+        </el-card>
+      </el-col>
+      <!-- 统计图 -->
+      <el-col :span="12">
+        <el-card class="box-card" shadow="never" style="height: 370px">
+          <div slot="header" class="clearfix">
+            <span>订单总数统计</span>
+            <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
+          </div>
+          <div ref="myChart" style="width: 100%;height: 270px"></div>
+        </el-card>
+      </el-col>
+    </el-row>
+
+    <el-row :gutter="20" class="my-3">
+      <el-col :span="12">
+        <el-card class="box-card" shadow="never" style="height: 300px">
+          <div slot="header" class="clearfix">
+            <span>销售情况统计</span>
+            <el-button style="float: right; padding: 3px 0" type="text">按周期统计商家店铺的订单量和订单金额</el-button>
+          </div>
+
+          <div class="media align-items-center border">
+            <span class="py-5 px-4 bg-light border-right">昨日销量</span>
+            <div class="media-body">
+              <div class="border-bottom pl-3 pb-3 mb-3">
+                <span>订单金额(元)  </span>12
+              </div>
+              <div class="pl-3">
+                <span>订单量(件) </span>12
+              </div>
+            </div>
+          </div>
+
+          <div class="media align-items-center border mt-3">
+            <span class="py-5 px-4 bg-light border-right">本月销量</span>
+            <div class="media-body">
+              <div class="border-bottom pl-3 pb-3 mb-3">
+                <span>订单金额(元)  </span>12
+              </div>
+              <div class="pl-3">
+                <span>订单量(件) </span>12
+              </div>
+            </div>
+          </div>
+        </el-card>
+      </el-col>
+      <el-col :span="12">
+        <el-card class="box-card">
+          <div slot="header" class="clearfix">
+            <span>单品销售排名</span>
+            <el-button style="float: right; padding: 3px 0" type="text">按周期统计商家店铺的订单量和订单金额</el-button>
+          </div>
+          <el-table
+              :data="tableData"
+              height="200"
+              border
+              style="width: 100%">
+            <el-table-column
+                type="index"
+                label="#"
+                width="50">
+            </el-table-column>
+            <el-table-column
+                prop="name"
+                label="商品信息">
+            </el-table-column>
+            <el-table-column
+                prop="num"
+                label="销量"
+                width="50">
+            </el-table-column>
+          </el-table>
+        </el-card>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
 <script>
+import echarts from 'echarts'
+
 export default {
-    name: "index"
+  name: "index",
+  data() {
+    return {
+      counts: [
+        {icon: 'el-icon-user-solid', color: 'bg-primary', num: 30, desc: '关注人数(个)'},
+        {icon: "el-icon-s-finance", desc: "订单总数(笔)", num: "120", color: "bg-success"},
+        {icon: "el-icon-s-order", desc: "今日订单总金额(元)", num: "4183.80", color: "bg-danger"},
+        {icon: "el-icon-s-data", desc: "本月销量(笔)", num: "100", color: "bg-warning"},
+      ],
+      tips: [
+        {
+          title: '店铺及商品提示',
+          desc: '需要关注的店铺信息及待处理事项',
+          list: [
+            {name: "出售中", value: "64"},
+            {name: "出售中", value: "64"},
+            {name: "出售中", value: "64"},
+            {name: "出售中", value: "64"},
+          ]
+        },
+        {
+          title: "店铺及商品提示",
+          desc: "需要关注的店铺信息及待处理事项",
+          list: [
+            {name: "出售中", value: "64"},
+            {name: "出售中", value: "64"},
+            {name: "出售中", value: "64"},
+            {name: "出售中", value: "64"},
+            {name: "出售中", value: "64"},
+            {name: "出售中", value: "64"},
+          ]
+        }
+      ],
+      tableData: [
+        {
+          name:"小天鹅（LittleSwan）滚筒洗衣机...",
+          num:"9"
+        },
+        {
+          name:"小天鹅（LittleSwan）滚筒洗衣机...",
+          num:"9"
+        },
+        {
+          name:"小天鹅（LittleSwan）滚筒洗衣机...",
+          num:"9"
+        },
+        {
+          name:"小天鹅（LittleSwan）滚筒洗衣机...",
+          num:"9"
+        },
+        {
+          name:"小天鹅（LittleSwan）滚筒洗衣机...",
+          num:"9"
+        }
+      ]
+    }
+  },
+  mounted() {
+    this.drawLine()
+  },
+  filters: {
+    getCol(total) {
+      return `col-${12 / total}`
+    }
+  },
+  methods: {
+    drawLine() {
+      // 初始化echarts实例
+      let myChart = echarts.init(this.$refs.myChart)
+      // 配置参数
+      myChart.setOption({
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'cross',
+            label: {
+              backgroundColor: '#6a7985'
+            }
+          }
+        },
+        legend: {
+          data: ['邮件营销', '联盟广告', '视频广告', '直接访问', '搜索引擎']
+        },
+        toolbox: {
+          feature: {
+            saveAsImage: {}
+          }
+        },
+        grid: {
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true
+        },
+        xAxis: [
+          {
+            type: 'category',
+            boundaryGap: false,
+            data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+          }
+        ],
+        yAxis: [
+          {
+            type: 'value'
+          }
+        ],
+        series: [
+          {
+            name: '邮件营销',
+            type: 'line',
+            stack: '总量',
+            areaStyle: {},
+            emphasis: {
+              focus: 'series'
+            },
+            data: [120, 132, 101, 134, 90, 230, 210]
+          },
+          {
+            name: '联盟广告',
+            type: 'line',
+            stack: '总量',
+            areaStyle: {},
+            emphasis: {
+              focus: 'series'
+            },
+            data: [220, 182, 191, 234, 290, 330, 310]
+          },
+          {
+            name: '视频广告',
+            type: 'line',
+            stack: '总量',
+            areaStyle: {},
+            emphasis: {
+              focus: 'series'
+            },
+            data: [150, 232, 201, 154, 190, 330, 410]
+          },
+          {
+            name: '直接访问',
+            type: 'line',
+            stack: '总量',
+            areaStyle: {},
+            emphasis: {
+              focus: 'series'
+            },
+            data: [320, 332, 301, 334, 390, 330, 320]
+          },
+          {
+            name: '搜索引擎',
+            type: 'line',
+            stack: '总量',
+            label: {
+              show: true,
+              position: 'top'
+            },
+            areaStyle: {},
+            emphasis: {
+              focus: 'series'
+            },
+            data: [820, 932, 901, 934, 1290, 1330, 1320]
+          }
+        ]
+      })
+    }
+  }
 }
 </script>
 
